dc201f8893b8e9fae1075a7c594ba313fe540f59
Florian Rohrer <florian.rohrer@vivaldi.net>
2018-02-27 16:37:28 +0100

    Tile refactored

--- src/components/board/Tile/Tile.tsx
+++ src/components/board/Tile/Tile.tsx
@@ -1,21 +1,15 @@
 import * as React from 'react';
 import 'material-design-icons/iconfont/material-icons.css';
 import { Player, TileValue } from '../../../state/AppState';
-import { XSymbol } from '../../symbols/XSymbol';
-import { OSymbol } from '../../symbols/OSymbol';
-import { Point } from '../../../util/Point';
-import { NoWinnerSymbol } from '../../symbols/NoWinnerSymbol';
+// import { XSymbol } from '../../symbols/XSymbol';
+// import { OSymbol } from '../../symbols/OSymbol';
+//import { NoWinnerSymbol } from '../../symbols/NoWinnerSymbol';
 import './tile.css';
 
-// TODO: Does this component have too many props?
 interface TileProps {
-    bigBoardPoint: Point; // Wouldn't need to know BigBoardPoint
-    smallBoardPoint?: Point;
-    onTileClicked?: ( bigBoard: Point, smallBoard: Point, player: Player, value: TileValue, allowed: boolean ) => void;
-    value?: TileValue;
-    player?: Player;
-    shouldDisplayIndicator?: boolean;
-    isSmallBoardFinished?: boolean;
+    onTileClicked: () => void;
+    value: TileValue;
+    currentPlayer: Player
 }
 
 interface TileState {
@@ -23,73 +17,69 @@ interface TileState {
 
 export class Tile extends React.Component<TileProps, TileState> {
 
-    public static defaultProps: Partial<TileProps> = {
-        shouldDisplayIndicator: false,
-        value: TileValue.Empty,
-    };
-
     constructor( props: TileProps ) {
         super( props );
 
-        this.getValue = this.getValue.bind( this );
-        this.handleClick = this.handleClick.bind( this );
-        this.getRoundnessClass = this.getRoundnessClass.bind( this );
+        // this.getValue = this.getValue.bind( this );
+        // this.handleClick = this.handleClick.bind( this );
+        // this.getRoundnessClass = this.getRoundnessClass.bind( this );
     }
 
-    getValue() {
-        const {value, isSmallBoardFinished} = this.props;
-
-        if (value === TileValue.Cross) {
-            return <XSymbol bigSymbol={isSmallBoardFinished!}/>;
-        } else if (value === TileValue.Circle) {
-            return <OSymbol bigSymbol={isSmallBoardFinished!}/>;
-        } else if (value === TileValue.Destroyed) {
-            return <NoWinnerSymbol/>;
-        } else {
-            return '';
-        }
-    }
-
-    handleClick() {
-        const {
-            bigBoardPoint, smallBoardPoint, player, value,
-            onTileClicked, shouldDisplayIndicator
-        } = this.props;
-        if (onTileClicked) {
-            onTileClicked( bigBoardPoint, smallBoardPoint!, player!, value!, shouldDisplayIndicator! );
-        }
-    }
-
-    getRoundnessClass() {
-        const {player, isSmallBoardFinished, value} = this.props;
-
-        let isCircle;
-        let isSquare;
-        if (isSmallBoardFinished) {
-            isCircle = value === TileValue.Circle;
-            isSquare = value === TileValue.Cross;
-        } else {
-            isCircle = player === Player.Circle;
-            isSquare = player === Player.Cross;
-        }
-
-        if (isCircle) {
-            return 'circle';
-        } else if (isSquare) {
-            return 'square';
-        } else {
-            return 'no-winner';
-        }
-    }
+    // getValue() {
+    //     const {value, isSmallBoardFinished} = this.props;
+    //
+    //     if (value === TileValue.Cross) {
+    //         return <XSymbol bigSymbol={isSmallBoardFinished!}/>;
+    //     } else if (value === TileValue.Circle) {
+    //         return <OSymbol bigSymbol={isSmallBoardFinished!}/>;
+    //     } else if (value === TileValue.Destroyed) {
+    //         return <NoWinnerSymbol/>;
+    //     } else {
+    //         return '';
+    //     }
+    // }
+    //
+    // handleClick() {
+    //     const {
+    //         bigBoardPoint, smallBoardPoint, player, value,
+    //         onTileClicked, shouldDisplayIndicator
+    //     } = this.props;
+    //     if (onTileClicked) {
+    //         onTileClicked( bigBoardPoint, smallBoardPoint!, player!, value!, shouldDisplayIndicator! );
+    //     }
+    // }
+    //
+    // getRoundnessClass() {
+    //     const {player, isSmallBoardFinished, value} = this.props;
+    //
+    //     let isCircle;
+    //     let isSquare;
+    //     if (isSmallBoardFinished) {
+    //         isCircle = value === TileValue.Circle;
+    //         isSquare = value === TileValue.Cross;
+    //     } else {
+    //         isCircle = player === Player.Circle;
+    //         isSquare = player === Player.Cross;
+    //     }
+    //
+    //     if (isCircle) {
+    //         return 'circle';
+    //     } else if (isSquare) {
+    //         return 'square';
+    //     } else {
+    //         return 'no-winner';
+    //     }
+    // }
 
     render() {
-        const tileValue = this.getValue();
-        const color = this.props.shouldDisplayIndicator ? 'indicator' : 'normal';
-        const roundnessClass = this.getRoundnessClass();
+        const {value, onTileClicked} = this.props;
+        //const tileValue = this.getValue();
+        const color = 'indicator';//this.props.shouldDisplayIndicator ? 'indicator' : 'normal';
+        const roundnessClass = 'circle';//this.getRoundnessClass();
 
         return (
-            <div className={`box ${color} ${roundnessClass}`} onClick={this.handleClick}>
-                {tileValue}
+            <div className={`box ${color} ${roundnessClass}`} onClick={onTileClicked}>
+                {value}
             </div>
         );
     }
