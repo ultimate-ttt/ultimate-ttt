62e4315685be0266f7c1585fc06d37e8524933b9
Florian Rohrer <florian.rohrer@vivaldi.net>
2018-02-27 16:41:24 +0100

    Tile Story Fix Layout

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,9 @@
 import { configure } from '@storybook/react'
 
 import '../src/index.css';
+import '../src/components/board/SmallBoard/smallboard.css';
+import '../src/components/board/Tile/tile.css';
+import '../src/components/board/BigBoard/bigboard.css';
 
 const req = require.context('../src', true, /\.story\.(ts|tsx)$/)
 
