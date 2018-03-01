4c3e4d2b89968441cb120dd97d16d1b9eaa71cb6
Florian Rohrer <florian.rohrer@vivaldi.net>
2018-03-01 09:59:03 +0100

    Bigboard added onPlayerMoved

--- src/state/game/gameAction.ts
+++ src/state/game/gameAction.ts
@@ -1,5 +1,30 @@
+import { Player } from '../AppState';
+
 export const CHANGE_PLAYER = 'game-reducer/player-changed';
-export const changePlayer = (  ) => ({
+export const PLAYER_MOVED = 'game/player-moved';
+export const PLAYER_MOVED_VALID = 'game/player-moved-valid';
+export const changePlayer = () => ({
     type: CHANGE_PLAYER,
-    payload: {  }
+    payload: {}
+});
+
+export const playerMoved = ( boardX: number, boardY: number, tileX: number, tileY: number ) => ({
+    type: PLAYER_MOVED,
+    payload: {
+        boardX,
+        boardY,
+        tileX,
+        tileY
+    }
+});
+
+export const playerMovedValid = ( boardX: number, boardY: number, tileX: number, tileY: number, player: Player ) => ({
+    type: PLAYER_MOVED,
+    payload: {
+        boardX,
+        boardY,
+        tileX,
+        tileY,
+        player
+    }
 });
