d7920f74f096ee13ab7cc9245b566979ceb8fc23
Florian Rohrer <florian.rohrer@vivaldi.net>
2018-03-19 11:59:08 +0100

    Added Todos

--- src/state/sagas/MoveValidationSaga.ts
+++ src/state/sagas/MoveValidationSaga.ts
@@ -2,22 +2,30 @@ import { put, select, takeEvery } from 'redux-saga/effects';
 import { changePlayer, PLAYER_MOVED, PlayerMovedAction } from '../game/gameAction';
 import { AppState } from '../AppState';
 import { registerMove } from '../moves/moveAction';
-import { setTileValue } from '../board/boardActions';
+import { setBoardValue, setTileValue } from '../board/boardActions';
 import { playerToTileValue } from '../../util';
 import { setActiveBoards } from '../activeBoards/activeBoardActions';
 
 const getCurrentPlayer = ( state: AppState ) => state.game.currentPlayer;
+// const getBoards = ( state: AppState ) => state.board;
 
 function* playerMoved( action: PlayerMovedAction ) {
     const {boardPoint, tilePoint} = action.payload;
     const currentPlayer = yield select( getCurrentPlayer );
+    // const boards = yield select( getBoards );
+
     const tileValue = playerToTileValue( currentPlayer );
 
     yield put( registerMove( boardPoint, tilePoint, currentPlayer ) );
     yield put( setTileValue( boardPoint, tilePoint, tileValue ) );
     yield put( changePlayer() );
 
-    yield put( setActiveBoards( [tilePoint] ) );
+    const isBoardWon = true; // TODO: do It correctly
+    if (isBoardWon) {
+        yield put( setBoardValue( boardPoint, currentPlayer ) );
+    }
+
+    yield put( setActiveBoards( [tilePoint] ) ); // TODO: check if all boards should be active
 }
 
 function* playerMovedSaga() {
