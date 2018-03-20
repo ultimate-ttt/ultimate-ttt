ba021197ae4840cf96c59d983ed6642144ae483e
maracuja-juice <mpanchaud@yahoo.de>
2018-03-20 15:29:22 +0100

    add xsymbol and nowinnersymbol tests

---
+++ src/components/symbols/test/OSymbol.test.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import * as ReactSixteenAdapter from 'enzyme-adapter-react-16';
+import { configure, shallow } from 'enzyme';
+import { OSymbol } from './OSymbol';
+
+configure( {adapter: new ReactSixteenAdapter()} );
+
+describe( 'OSymbol', () => {
+    it( 'should not explode', () => {
+        const component = shallow( <OSymbol/> );
+        expect( component ).not.toBeNull();
+    } );
+
+    it( 'should have i element with some kind of icon', () => {
+        const component = shallow( <OSymbol/> );
+        expect( component.find( 'i' ) ).toHaveLength( 1 );
+        expect( component.find( 'i' ).text() ).not.toBeUndefined();
+    } );
+
+    it( 'should have material-icons, o and animate-o class by default', () => {
+        const component = shallow( <OSymbol/> );
+        expect( component.find( '.material-icons .o .animate-o' ) ).toHaveLength( 1 );
+        expect( component.find( '.big-symbol' ) ).toHaveLength( 0 );
+    } );
+
+    it( 'should have animate-o-big-symbol class with and bigSymbol=true', () => {
+        const component = shallow( <OSymbol bigSymbol={true}/> );
+        expect( component.find( '.animate-o-big-symbol' ) ).toHaveLength( 1 );
+        expect( component.find( '.animate-o' ) ).toHaveLength( 0 );
+        expect( component.find( '.big-symbol' ) ).toHaveLength( 1 );
+    } );
+} );
---
+++ src/components/symbols/test/XSymbol.test.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import * as ReactSixteenAdapter from 'enzyme-adapter-react-16';
+import { configure, shallow } from 'enzyme';
+import { XSymbol } from './XSymbol';
+
+configure( {adapter: new ReactSixteenAdapter()} );
+
+describe( 'XSymbol', () => {
+    it( 'should not explode', () => {
+        const component = shallow( <XSymbol/> );
+        expect( component ).not.toBeNull();
+    } );
+
+    it( 'should have i element with some kind of icon', () => {
+        const component = shallow( <XSymbol/> );
+        expect( component.find( 'i' ) ).toHaveLength( 1 );
+        expect( component.find( 'i' ).text() ).not.toBeUndefined();
+    } );
+
+    it( 'should have material-icons, x and animate-x class by default', () => {
+        const component = shallow( <XSymbol/> );
+        expect( component.find( '.material-icons .x .animate-x' ) ).toHaveLength( 1 );
+        expect( component.find( '.big-symbol' ) ).toHaveLength( 0 );
+    } );
+
+    it( 'should have animate-x-big-symbol class with and bigSymbol=true', () => {
+        const component = shallow( <XSymbol bigSymbol={true}/> );
+        expect( component.find( '.animate-x-big-symbol' ) ).toHaveLength( 1 );
+        expect( component.find( '.animate-x' ) ).toHaveLength( 0 );
+        expect( component.find( '.big-symbol' ) ).toHaveLength( 1 );
+    } );
+} );
