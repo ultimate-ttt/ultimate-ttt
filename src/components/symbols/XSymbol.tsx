e5034a4bee18ccb86e7516a173a59505d9bc156c
maracuja-juice <mpanchaud@yahoo.de>
2018-03-20 11:32:15 +0100

    remove unneeded shouldAnimate property

--- src/components/symbols/XSymbol.tsx
+++ src/components/symbols/XSymbol.tsx
@@ -5,30 +5,24 @@ import * as classNames from 'classnames';
 
 interface XSymbolProps {
     bigSymbol?: boolean;
-    shouldAnimate?: boolean;
 }
 
 interface XSymbolState {
 }
 
 export class XSymbol extends React.Component<XSymbolProps, XSymbolState> {
-
-    public static defaultProps: Partial<XSymbolProps> = {
-        shouldAnimate: true
-    };
-
+    
     constructor( props: XSymbolProps ) {
         super( props );
     }
 
     render() {
-        // TODO: is there a case where it shouldn't animate?
-        const {bigSymbol, shouldAnimate} = this.props;
+        const {bigSymbol} = this.props;
         var iconClass = classNames( {
                                         'material-icons x': true,
                                         'big-symbol': bigSymbol,
-                                        'animate-x': shouldAnimate && !bigSymbol,
-                                        'animate-x-big-symbol': shouldAnimate && bigSymbol
+                                        'animate-x': !bigSymbol,
+                                        'animate-x-big-symbol': bigSymbol
                                     } );
         return (
             <i className={iconClass}>clear</i>
