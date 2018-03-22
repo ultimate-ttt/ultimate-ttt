2f8dcb8c69f6d5eedb917cc035c139b4b99765cd
maracuja-juice <mpanchaud@yahoo.de>
2018-03-22 19:27:25 +0100

    use unfinished board mock in board calculation

--- src/state/sagas/test/boardCalculationSaga.test.ts
+++ src/state/sagas/test/boardCalculationSaga.test.ts
@@ -3,27 +3,27 @@ import boardCalculationSaga from '../boardCalculationSaga';
 import { getBoards } from '../../selectors/AppStateSelectors';
 import { select } from 'redux-saga/effects';
 import { CALCULATE_BOARD_VALUE, SET_BOARD_VALUE } from '../../board/boardActions';
-import boardStateMock from './finishedBoardMock';
 import { Player } from '../../AppState';
+import unfinishedBoardMock from './unfinishedBoardMock';
 
 describe( 'boardCalculationSaga', () => {
     it( 'should dispatch the set board value for the board with the Cross Player', () => {
         return expectSaga( boardCalculationSaga )
             .provide([
-                         [select(getBoards), boardStateMock]
+                         [select(getBoards), unfinishedBoardMock]
                      ])
-            .put({type: SET_BOARD_VALUE, payload: {boardPosition: {x: 1, y: 0}, tileValue: Player.Cross}})
-            .dispatch( {type: CALCULATE_BOARD_VALUE, payload: {x: 1, y: 0}} ) // TODO maybe this needs to be switched to 0,1
+            .put({type: SET_BOARD_VALUE, payload: {boardPosition: {x: 0, y: 1}, tileValue: Player.Cross}})
+            .dispatch( {type: CALCULATE_BOARD_VALUE, payload: {x: 0, y: 1}} ) // TODO maybe this needs to be switched to 1,0
             .run();
     } );
 
     it( 'should dispatch the set board value for the board with the Circle Player', () => {
         return expectSaga( boardCalculationSaga )
             .provide([
-                         [select(getBoards), boardStateMock]
+                         [select(getBoards), unfinishedBoardMock]
                      ])
-            .put({type: SET_BOARD_VALUE, payload: {boardPosition: {x: 0, y: 1}, tileValue: Player.Circle}})
-            .dispatch( {type: CALCULATE_BOARD_VALUE, payload: {x: 0, y: 1}} ) // TODO maybe this needs to be switched to 1,0
+            .put({type: SET_BOARD_VALUE, payload: {boardPosition: {x: 1, y: 1}, tileValue: Player.Circle}})
+            .dispatch( {type: CALCULATE_BOARD_VALUE, payload: {x: 1, y: 1}} )
             .run();
     } );
 
@@ -31,9 +31,9 @@ describe( 'boardCalculationSaga', () => {
     it('should match snapshot', () => {
         return expectSaga(boardCalculationSaga)
             .provide([
-                         [select(getBoards), boardStateMock]
+                         [select(getBoards), unfinishedBoardMock]
                      ])
-            .dispatch( {type: CALCULATE_BOARD_VALUE, payload: {x: 2, y: 1}} )
+            .dispatch( {type: CALCULATE_BOARD_VALUE, payload: {x: 0, y: 0}} )
             .run()
             .then((result) => {
                 expect(result.toJSON()).toMatchSnapshot();
