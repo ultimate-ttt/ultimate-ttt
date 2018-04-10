b2434f6c5755669da029a20879cbe47752d863b7
maracuja-juice <mpanchaud@yahoo.de>
2018-04-10 23:34:35 +0200

    Update style of Game Finished Component
    This way it looks less out of place. This is only a work in progress
    design though.

--- src/components/symbols/OSymbol.tsx
+++ src/components/symbols/OSymbol.tsx
@@ -5,23 +5,29 @@ import * as classNames from 'classnames';
 
 interface OSymbolProps {
     bigSymbol?: boolean;
+    shouldAnimate?: boolean;
 }
 
 interface OSymbolState {
 }
 
+// TODO add new tests
 export class OSymbol extends React.Component<OSymbolProps, OSymbolState> {
+    public static defaultProps: Partial<OSymbolProps> = {
+        shouldAnimate: true
+    };
+
     constructor( props: OSymbolProps ) {
         super( props );
     }
 
     render() {
-        const {bigSymbol} = this.props;
+        const {bigSymbol, shouldAnimate} = this.props;
         var iconClass = classNames( {
                                         'material-icons o': true,
                                         'big-symbol': bigSymbol,
-                                        'animate-o': !bigSymbol,
-                                        'animate-o-big-symbol': bigSymbol
+                                        'animate-o': !bigSymbol && shouldAnimate,
+                                        'animate-o-big-symbol': bigSymbol && shouldAnimate
                                     } );
 
         return (
