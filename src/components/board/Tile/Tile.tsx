0e32ca67a81c1d78e8cdf65d2447acdebeb63806
maracuja-juice <mpanchaud@yahoo.de>
2018-04-12 21:48:40 +0200

    rename isCircle to isTileRound

--- src/components/board/Tile/Tile.tsx
+++ src/components/board/Tile/Tile.tsx
@@ -10,7 +10,7 @@ import './tile.css';
 interface TileProps {
     onTileClicked?: () => void;
     value: TileValue;
-    isCircle: boolean;
+    isTileRound: boolean;
     isClickable: boolean;
     isBig?: boolean;
 }
@@ -41,10 +41,10 @@ export class Tile extends React.Component<TileProps, TileState> {
     }
 
     render() {
-        const {onTileClicked, isCircle, isClickable, value} = this.props;
+        const {onTileClicked, isTileRound, isClickable, value} = this.props;
         const color = isClickable ? 'indicator' : 'normal';
 
-        let roundness = isCircle ? 'circle' : 'square';
+        let roundness = isTileRound ? 'circle' : 'square';
         if (value === TileValue.Destroyed) {
             roundness = 'no-winner';
         }
