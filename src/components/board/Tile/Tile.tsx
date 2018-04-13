f43bed1017786c26cd6cd7eb2c75d6f1cb4452d0
maracuja-juice <16801528+maracuja-juice@users.noreply.github.com>
2018-04-13 18:45:25 +0200

    change icon font
    generated by icomoon, smaller font-size

--- src/components/board/Tile/Tile.tsx
+++ src/components/board/Tile/Tile.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
-import 'material-design-icons/iconfont/material-icons.css';
+import './tile.css';
 import { TileValue } from '../../../state/AppState';
 import { XSymbol } from '../../symbols/XSymbol';
 import { OSymbol } from '../../symbols/OSymbol';
 import { NoWinnerSymbol } from '../../symbols/NoWinnerSymbol';
-
-import './tile.css';
 
 interface TileProps {
     onTileClicked?: () => void;
