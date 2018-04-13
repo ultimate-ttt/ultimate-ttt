61dd0a8d8d0189437b3c489b1aed9d05187aac39
maracuja-juice <16801528+maracuja-juice@users.noreply.github.com>
2018-04-13 18:45:25 +0200

    change icon font
    generated by icomoon, smaller font-size

--- src/components/symbols/XSymbol.tsx
+++ src/components/symbols/XSymbol.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './symbols.css';
 import './symbolAnimations.css';
+import '../../fonts/icons.css';
 import * as classNames from 'classnames';
 
 interface XSymbolProps {
@@ -19,7 +20,7 @@ export class XSymbol extends React.Component<XSymbolProps, XSymbolState> {
     render() {
         const {bigSymbol} = this.props;
         var iconClass = classNames( {
-                                        'material-icons x': true,
+                                        'icon-cross x': true,
                                         'big-symbol': bigSymbol,
                                         'animate-x': !bigSymbol,
                                         'animate-x-big-symbol': bigSymbol
