487a95f4bcabb286f8bf10ccbdfb91b0374b7cf9
maracuja-juice <16801528+maracuja-juice@users.noreply.github.com>
2018-10-27 00:11:05 +0200

    add selectors, and most action types needed for new saga

--- src/state/game/gameAction.ts
+++ src/state/game/gameAction.ts
@@ -1,12 +1,12 @@
 import { Player } from '../AppState';
 import { Point } from '../../util/Point';
 
-export const CHANGE_PLAYER = 'game-reducer/player-changed';
+export const CHANGE_PLAYER = 'game/player-changed';
 export const PLAYER_MOVED = 'game/player-moved';
 export const PLAYER_MOVED_VALID = 'game/player-moved-valid';
 export const GAME_FINISHED = 'game/game-finished';
 export const CHECK_GAME_FINISHED = 'game/check-game-finished';
-export const SAVE_GAME_STATE_TO_SERVER = 'game/save-game-state-to-server';
+export const SAVE_FINISHED_GAME_DATA = 'game/save-finished-game-data';
 
 export const changePlayer = () => ({
     type: CHANGE_PLAYER
@@ -21,8 +21,8 @@ export const checkGameFinished = (  ) => ({
     type: CHECK_GAME_FINISHED
 });
 
-export const saveGameStateToServer = () => ({
-    type: SAVE_GAME_STATE_TO_SERVER
+export const saveFinishedGameData = () => ({
+    type: SAVE_FINISHED_GAME_DATA
 });
 
 export const playerMoved = ( boardPosition: Point, tilePosition: Point ): PlayerMovedAction => ({
--- src/state/selectors/AppStateSelectors.ts
+++ src/state/selectors/AppStateSelectors.ts
@@ -1,5 +1,23 @@
 import { AppState } from '../AppState';
+import { createSelector } from 'reselect';
 
 export const getBoards = ( state: AppState ) => state.board;
 
 export const getCurrentPlayer = ( state: AppState ) => state.game.currentPlayer;
+
+export const getWinningPlayer = ( state: AppState ) => state.game.winningPlayer;
+
+export const getIsReplay = ( state: AppState ) => state.game.isReplay;
+
+export const getMoves = ( state: AppState ) => state.moves;
+
+export const getFinishedGameData = createSelector(
+    [getWinningPlayer, getIsReplay, getBoards, getMoves],
+    (winningPlayer, isReplay, boards, moves) => {
+        return {
+            winner: winningPlayer,
+            gameState: boards,
+            isReplay,
+            moves
+        }
+    });
