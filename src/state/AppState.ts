a8eb027c07c2e4c79fd936d19e56cf099c01ae9b
Florian Rohrer <florian.rohrer@vivaldi.net>
2018-10-29 12:37:51 +0100

    removed undefined from winningPlayer type

--- src/state/AppState.ts
+++ src/state/AppState.ts
@@ -11,7 +11,7 @@ export interface GameState {
     currentPlayer: Player;
     isFinished: boolean;
     isReplay: boolean;
-    winningPlayer?: Player | undefined | null;
+    winningPlayer?: Player | null;
 }
 
 export interface TileInformation {
