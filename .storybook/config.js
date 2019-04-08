d1e581af334d41ae98733ea17bf3e00b265c85ee
maracuja-juice <16801528+maracuja-juice@users.noreply.github.com>
2019-04-08 21:18:18 +0200

    add rmwc icon css to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -8,6 +8,7 @@ import '../src/views/Analysis/AnalysisGame.css';
 
 import '@material/button/dist/mdc.button.min.css';
 import '@material/list/dist/mdc.list.css';
+import '@rmwc/icon/icon.css';
 
 const req = require.context('../src', true, /\.story\.(ts|tsx)$/)
 
