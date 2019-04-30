904a6a7f107909d3f183073a4408bdde8ccd880d
maracuja-juice <16801528+maracuja-juice@users.noreply.github.com>
2019-04-30 23:23:18 +0200

    implement initial paging component

--- src/components/Paging/Paging.story.tsx
+++ src/components/Paging/Paging.story.tsx
@@ -3,22 +3,25 @@ import { storiesOf } from '@storybook/react';
 import { number, withKnobs } from '@storybook/addon-knobs';
 import { Paging } from './Paging';
 import { action } from '@storybook/addon-actions';
+import { IconProvider } from '../IconProvider';
 
 const stories = storiesOf('Paging', module);
 stories.addDecorator(withKnobs);
 
 stories.add('Paging Standard', () => (
-  <Paging
-    pages={number('pages', 10)}
-    currentPage={number('currentPage', 5)}
-    onPageChange={action('onPageChange')}
-  />
+  <IconProvider>
+    <Paging
+      pages={number('pages', 10)}
+      pageToStartWith={number('pageToStartWith', 5)}
+      onPageChange={action('onPageChange')}
+    />
+  </IconProvider>
 ));
 
 stories.add('Paging without all Pages', () => (
   <Paging
     pages={number('pages', 10)}
-    currentPage={number('currentPage', 5)}
+    pageToStartWith={number('pageToStartWith', 5)}
     onPageChange={action('onPageChange')}
     pagesToDisplayFrom={number('pagesToDisplayFrom', 1)}
     pagesToDisplayTo={number('pagesToDisplayTo', 8)}
--- src/components/Paging/Paging.tsx
+++ src/components/Paging/Paging.tsx
@@ -1,18 +1,43 @@
 import * as React from 'react';
 import { Button } from '@rmwc/button';
+import styles from './Paging.module.css';
+import { useState } from 'react';
 
 interface PagingProps {
   pages: number;
-  currentPage: number;
+  pageToStartWith: number;
   onPageChange: (/* lastPage: number, */ newPage: number) => void;
   pagesToDisplayFrom?: number;
   pagesToDisplayTo?: number;
 }
 
 export function Paging(props: PagingProps) {
-  return (
-    <Button raised={false} dense={true}>
-      1
-    </Button>
-  );
+  const { pages, onPageChange } = props;
+
+  const [currentPage, setCurrentPage] = useState(props.pageToStartWith);
+
+  const getPages = () => {
+    const pageButtons: JSX.Element[] = [];
+    for (let i = 1; i < pages + 1; i++) {
+      pageButtons.push(
+        <Button
+          key={i}
+          label={i}
+          onClick={() => {
+            if (i !== currentPage) {
+              setCurrentPage(i);
+              onPageChange(i);
+            }
+          }}
+          dense={true}
+          ripple={false}
+          outlined={i === currentPage}
+          className={styles.pagingButton}
+        />,
+      );
+    }
+    return pageButtons;
+  };
+
+  return <div>{getPages()}</div>;
 }
