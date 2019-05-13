49b00e06b9cd01ec6f1f8025de4f7cbd70f4cf03
Maurice <16801528+maracuja-juice@users.noreply.github.com>
2019-05-13 10:11:53 +0200

    add no game found info when no game was found (#195)
    * add no game found info when no game was found
    * improve display of no recently played games
    * change children type to string

---
+++ src/views/Analysis/NoGameFound.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Typography, TypographyT } from '@rmwc/typography';
+import styles from './NoGameFound.module.css';
+import classNames from 'classnames';
+
+interface NoGameFoundProps {
+  tag: string;
+  center?: boolean;
+  className?: string;
+  children?: string;
+  size?: TypographyT;
+}
+
+export function NoGameFound(props: NoGameFoundProps) {
+  return (
+    <div
+      className={classNames(props.className, {
+        [styles.noGameFoundLayout]: props.center,
+      })}
+    >
+      <Typography use={props.size ? props.size : 'headline3'} tag={props.tag}>
+        {props.children ? props.children : 'No game was found'}
+      </Typography>
+    </div>
+  );
+}
