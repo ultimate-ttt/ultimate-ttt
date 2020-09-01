718da8c4eed719d46a0e8556ba1bc59fa63c2f28
maracuja-juice <16801528+maracuja-juice@users.noreply.github.com>
2020-09-01 14:01:32 +0200

    update react-scripts and typescript

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,8 +1,8 @@
-import { Point, arePointsEqual } from './Point';
+import { Point as P, arePointsEqual } from './Point';
 import { playerToTileValue } from './PlayerToTile';
 import { TicTacToeGame } from './TicTacToeGame';
 import { getApiUrl } from './ApiUrl';
 
 export { playerToTileValue, arePointsEqual, TicTacToeGame, getApiUrl };
 // https://github.com/facebook/create-react-app/issues/6054
-export type Point = Point;
+export type Point = P;
