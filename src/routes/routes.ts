f053ebcf6650a6c0faa283d2d59e6c617d10b53f
Maurice Panchaud <16801528+panmau@users.noreply.github.com>
2021-05-14 01:13:44 +0200

    refactor navigation (#248)
    * refactor Navigation component

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,8 @@ const routes = {
   AnalysisOverview: '/analysis',
   Home: '/',
   GitHub: 'https://github.com/ultimate-ttt/ultimate-ttt',
+  GitHubBug:
+    'https://github.com/ultimate-ttt/ultimate-ttt/issues/new?template=bug_report.md',
 };
 
 export default routes;
