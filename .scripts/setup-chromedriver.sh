44998212429439e6b9e7489522672d39f79d1479
panmau <16801528+panmau@users.noreply.github.com>
2021-06-20 15:15:53 +0200

    try with different script header

--- .scripts/setup-chromedriver.sh
+++ .scripts/setup-chromedriver.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Taken from https://github.com/nanasess/setup-chromedriver/blob/master/lib/setup-chromedriver.sh
 
@@ -6,21 +6,28 @@ set -e
 
 ARCH=$1
 
-if "$ARCH" == "linux64"
-then
+
+if [ "$ARCH" == "" ]; then
+  echo "You need to supply the Architecture parameter"
+  exit 1
+fi
+
+if [ "$ARCH" == "linux64" ]; then
     CHROMEAPP=google-chrome
     if ! type -a google-chrome > /dev/null 2>&1;
-     then
+    then
         sudo apt-get update
         sudo apt-get install -y google-chrome
     fi
-elif "$ARCH" == "mac64"
-then
+  
+elif [ "$ARCH" == "mac64" ]; then
     CHROMEAPP="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 fi
 
 CHROME_VERSION=$("$CHROMEAPP" --version | cut -f 3 -d ' ' | cut -d '.' -f 1)
+echo $CHROME_VERSION
 VERSION=$(curl --location --fail --retry 10 http://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION})
+echo $VERSION
 
 wget -c -nc --retry-connrefused --tries=0 https://chromedriver.storage.googleapis.com/${VERSION}/chromedriver_${ARCH}.zip
 unzip -o -q chromedriver_${ARCH}.zip
