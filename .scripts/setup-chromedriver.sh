69e4d4beca8c38ba6e39495aa98b0eae4e72bce8
panmau <16801528+panmau@users.noreply.github.com>
2021-06-20 15:02:56 +0200

    try out sh differently

--- .scripts/setup-chromedriver.sh
+++ .scripts/setup-chromedriver.sh
@@ -6,13 +6,16 @@ set -e
 
 ARCH=$1
 
-if [ "$ARCH" == "linux64" ]; then
+if "$ARCH" == "linux64"
+then
     CHROMEAPP=google-chrome
-    if ! type -a google-chrome > /dev/null 2>&1; then
+    if ! type -a google-chrome > /dev/null 2>&1;
+     then
         sudo apt-get update
         sudo apt-get install -y google-chrome
     fi
-elif [ "$ARCH" == "mac64" ]; then
+elif "$ARCH" == "mac64"
+then
     CHROMEAPP="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 fi
 
