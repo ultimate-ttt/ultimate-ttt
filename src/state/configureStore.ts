9c7003dd23fed38dca3be6ca0a967c2b44c888aa
panmau <16801528+panmau@users.noreply.github.com>
2021-06-26 12:20:46 +0200

    rename storage

--- src/state/configureStore.ts
+++ src/state/configureStore.ts
@@ -2,7 +2,7 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import boardReducer from './currentGame/board/boardReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import localStorage from 'redux-persist/lib/storage';
 import gameReducer from './currentGame/game/gameReducer';
 import moveReducer from './currentGame/moves/moveReducer';
 import activeBoardsReducer from './currentGame/activeBoards/activeBoardsReducer';
@@ -47,7 +47,7 @@ export function configureStore() {
   const persistConfig = {
     key: 'finishedGames',
     whitelist: ['finishedGames'],
-    storage,
+    storage: localStorage,
   };
 
   const persistedReducer = persistReducer(persistConfig, rootReducer);
